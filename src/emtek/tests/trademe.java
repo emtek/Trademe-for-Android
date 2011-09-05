3fbaba014ab765e25597dd79307613b52c821733
emtek <slowtalkinguy@gmail.com>
2011-09-05 22:39:25 +1200

    with new webview goodness

--- src/emtek/tests/trademe.java
+++ src/emtek/tests/trademe.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.URI;
+import java.util.Date;
 import java.util.Map;
 import java.util.Scanner;
 import java.util.Stack;
@@ -49,6 +50,8 @@ import android.text.ClipboardManager;
 import android.text.Editable;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ArrayAdapter;
@@ -83,13 +86,91 @@ public class trademe extends Activity {
 		try {
 			url = provider.retrieveRequestToken(consumer, "oob");
 			Uri uri = Uri.parse(url);
-	        Intent intent = new Intent(Intent.ACTION_VIEW, uri);
-	        startActivity(intent);
+			 WebView webview = new WebView(this);
+			 webview.setWebViewClient(new WebViewClient(){
+				 public void onPageFinished(WebView view , String url){
+					 if(url.contains("veri")){
+						 setContentView(R.layout.main);
+						 String[] values = url.split("&oauth_verifier=");
+						 String verifier = values[1].substring(0, 7);
+						 try {
+								provider.retrieveAccessToken(consumer, verifier);
+								
+								File cachegories = new File(getCacheDir() + "/Watchlist.json");
+								if((!cachegories.exists())||(cachegories.lastModified()< (System.currentTimeMillis()-600000))){
+									HttpGet request = new HttpGet("https://api.trademe.co.nz/v1/MyTradeMe/Watchlist.json");
+									HttpClient httpClient = new DefaultHttpClient();
+									consumer.sign(request);
+									try {
+										
+										HttpResponse response = httpClient.execute(request);
+										InputStream inStream = response.getEntity().getContent();
+										FileOutputStream outStream = new FileOutputStream(cachegories, false);
+										byte[] data = new byte[10240];
+										int len = 0;
+										while((len = inStream.read(data))>0){
+											outStream.write(data, 0, len);
+										}
+										outStream.close();
+										inStream.close();
+									} catch (ClientProtocolException e) {
+										// TODO Auto-generated catch block
+										
+										e.printStackTrace();
+									} catch (IOException e) {
+										// TODO Auto-generated catch block
+										
+										e.printStackTrace();
+									}
+								}else{
+									
+									
+								}
+								
+								FileReader fRead = null;
+								try {
+									fRead = new FileReader(cachegories);
+									
+									Gson gson = new Gson();
+									final WatchList root = gson.fromJson(fRead, WatchList.class);
+									Thread.sleep(1000);
+								} catch (FileNotFoundException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								} catch (IOException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								} catch (InterruptedException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
+								
+							} catch (OAuthMessageSignerException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							} catch (OAuthNotAuthorizedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							} catch (OAuthExpectationFailedException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							} catch (OAuthCommunicationException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+							
+						  }
+					 }
+				 });
+			 
+			 setContentView(webview);
+			 webview.loadUrl(url);
+
 		} catch (OAuthMessageSignerException e) {
 			// TODO Auto-generated catch block with a difference
 			e.printStackTrace();
 		} catch (OAuthNotAuthorizedException e) {
-			// TODO Auto-generated catch block
+			// TODO Login again
 			e.printStackTrace();
 		} catch (OAuthExpectationFailedException e) {
 			// TODO Auto-generated catch block
@@ -103,104 +184,8 @@ public class trademe extends Activity {
     
 	public void onResume(){
 		super.onResume();
-		showDialog(VERIFICATION_DIALOG);
+		//showDialog(VERIFICATION_DIALOG);
 	}
-	public Dialog onCreateDialog(int id){
-		if(id == VERIFICATION_DIALOG)
-			return createVerifyDialog(); 
-		else
-			return null;
-	}
-	
-	
-	
-	private AlertDialog createVerifyDialog(){
-		AlertDialog.Builder alert = new AlertDialog.Builder(this);
-		
-		alert.setTitle("Verification");
-		alert.setMessage("Paste verification code");
-		
-		final EditText input = new EditText(this);
-		alert.setView(input);
-		alert.setPositiveButton("OK", new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int whichButton) {
-				  Editable value = input.getText();
-				  	try {
-						provider.retrieveAccessToken(consumer, value.toString());
-						
-						File cachegories = new File(getCacheDir() + "/Watchlist.json");
-						if(!cachegories.exists()){
-							HttpGet request = new HttpGet("https://api.trademe.co.nz/v1/MyTradeMe/Watchlist.json");
-							HttpClient httpClient = new DefaultHttpClient();
-							consumer.sign(request);
-							try {
-								
-								HttpResponse response = httpClient.execute(request);
-								InputStream inStream = response.getEntity().getContent();
-								FileOutputStream outStream = new FileOutputStream(cachegories, false);
-								byte[] data = new byte[10240];
-								int len = 0;
-								while((len = inStream.read(data))>0){
-									outStream.write(data, 0, len);
-								}
-								outStream.close();
-								inStream.close();
-							} catch (ClientProtocolException e) {
-								// TODO Auto-generated catch block
-								
-								e.printStackTrace();
-							} catch (IOException e) {
-								// TODO Auto-generated catch block
-								
-								e.printStackTrace();
-							}
-						}else{
-							
-							
-						}
-						
-						FileReader fRead = null;
-						try {
-							fRead = new FileReader(cachegories);
-							
-							Gson gson = new Gson();
-							final WatchList root = gson.fromJson(fRead, WatchList.class);
-							Thread.sleep(1000);
-						} catch (FileNotFoundException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						} catch (IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						} catch (InterruptedException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-						
-					} catch (OAuthMessageSignerException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (OAuthNotAuthorizedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (OAuthExpectationFailedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					} catch (OAuthCommunicationException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-					dismissDialog(VERIFICATION_DIALOG);
-				  }
-				});
-		alert.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-			  public void onClick(DialogInterface dialog, int whichButton) {
-				    // Canceled.
-				  }});
-
-		return alert.create();
-	}
-	
 	
 }
     
